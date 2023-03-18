0d9244e898a268e79b6c72186c30ce1191d5d613
Guilherme Souza <guilhermejcamargos@gmail.com>
2023-03-17 22:29:32 -0300

    service

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,11 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { Moment } from 'src/app/Moments';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
-  styleUrls: ['./moment-form.component.css']
+  styleUrls: ['./moment-form.component.css'],
 })
-export class MomentFormComponent {
-  @Input() btnText!: string;
+export class MomentFormComponent implements OnInit {
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      image: new FormControl(''),
+    });
+  }
 
+  @Output() onSubmit = new EventEmitter<Moment>();
+  @Input() btnText!: string;
+  momentForm!: FormGroup;
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    this.momentForm.patchValue({ image: file });
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+    console.log(this.momentForm.value);
+
+    this.onSubmit.emit(this.momentForm.value)
+  }
 }
